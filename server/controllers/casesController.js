718622a2c6de5799f7096c49db800d15fc52fb7a
cryptodoctor <emmanuelneni@gmail.com>
2020-06-29 15:41:43 +0300

    created case adding controller

---
+++ server/controllers/casesController.js
@@ -0,0 +1,25 @@
+const Case = require('../models/casesModel')
+
+exports.addNewCase = async (req, res, next) => {
+  try {
+    const caseData = new Case({
+      owners_name: req.body.owners_name,
+      bike_description: req.body.bike_description,
+      type: req.body.type,
+      theft_date: req.body.theft_date,
+      theft_location: req.body.theft_location,
+      theft_description: req.body.theft_description,
+      status: 'pending',
+      _officerId: null,
+    })
+    const newCase = await caseData.save()
+
+    res.status(200).json({
+      type: 'success',
+      message: 'Case Added successfully!',
+      newCase,
+    })
+  } catch (error) {
+    res.status(500).json({ type: 'error', error })
+  }
+}
